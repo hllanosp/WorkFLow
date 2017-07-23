09785cfbb96807c643a5f26d4b70cd04bf6bd503
hllanosp <hllanos75@gmail.com>
2017-07-22 22:31:11 -0600

    actualizando repositorio con el que se encuentra alojado

--- conexion/conn.php
+++ conexion/conn.php
@@ -1,13 +1,9 @@
 <?php
- // $db = null;
- // $host = '158.69.241.209';
- // $dbname = 'hiwypani_workflow2';
- // $username = 'hiwypani_hllanos';
- // $password = 'Poporoyl@666';
-
  $db = null;
- $host = 'localhost';
- $dbname = 'workflow';
- $username = 'root';
- $password = '';
+ $host = '158.69.241.209';
+ $dbname = 'hiwypani_workflow3';
+ $username = 'hiwypani_hllanos';
+ $password = 'Poporoyla666';
+
+ 
 ?>
